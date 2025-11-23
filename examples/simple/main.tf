3e05bf494d17c5a8c62efd437098563b39249318
aarti.joshi <aarti.joshi@nttdata.com>
2025-11-23 15:48:06 -0500

    Naming module call change

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -11,7 +11,7 @@
 // limitations under the License.
 
 module "naming" {
-  source  = "terraform.registry.launch.nttdata.com/module_library/resource_name/aws"
+  source  = "terraform.registry.launch.nttdata.com/module_library/resource_name/launch"
   version = "~> 2.0"
 
   logical_product_family  = var.logical_product_family
